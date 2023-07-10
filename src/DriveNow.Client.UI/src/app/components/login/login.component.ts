fe772cdf21f889b38371e2eb6471a06f3916abfb
Andrew-CarSharing <obertynskiya@ukr.net>
2023-07-10 15:07:42 +0300

    AM_19_Google

--- src/DriveNow.Client.UI/src/app/components/login/login.component.ts
+++ src/DriveNow.Client.UI/src/app/components/login/login.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {PromptMomentNotification} from "google-one-tap";
+import {GoogleLoginProvider, SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
+import {AuthService} from "../../services/auth.service";
+import {UserFromGoogle} from "../../interfaces/user-from-google";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  constructor(
+      private auth: AuthService,
+      private socialAuthService: SocialAuthService
+  ) {
+  }
+  // @ts-ignore
+  login: FormGroup;
 
+  ngOnInit() {
+    this.socialAuthService.authState.subscribe((user)=>{
+      this.socialUser = user;
+      console.log(this.socialUser);
+      var user_google = new UserFromGoogle;
+      user_google.firstName = user.firstName;
+      user_google.secondName = user.lastName;
+      user_google.email = user.email;
+      this.auth.loginWithGoogle(user_google).subscribe(
+
+      );
+    })
+  }
+
+  socialUser!: SocialUser;
+  loginWithGoogle(){
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
 }
---
+++ src/DriveNow.Client.UI/src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AUTH_API_URL} from "../app.injection-tokens";
+import {UserFromGoogle} from "../interfaces/user-from-google";
+import {Observable, tap} from "rxjs";
+import {Token} from "../interfaces/token";
+import {Router} from "@angular/router";
+export const ACCESS_TOKEN_KEY = 'bookstore_access_token';
+
+class User {
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient,
+              @Inject(AUTH_API_URL) private apiUrl: string,
+              private router: Router
+              ) { }
+
+  public loginWithGoogle(user_from_google: UserFromGoogle): Observable<Token>{
+    return this.http.post<Token>(`${this.apiUrl}`,user_from_google).pipe(
+        tap(token=>{
+          localStorage.setItem(ACCESS_TOKEN_KEY,token.access_token);
+        })
+    )
+  }
+
+  public login(User: User):Observable<Token>{
+      return this.http.post<Token>(`${this.apiUrl}SingIn/SingInUser`,User).pipe(
+          tap(token=>{
+              localStorage.setItem(ACCESS_TOKEN_KEY,token.access_token);
+          })
+      )
+  }
+
+  public logout(): void{
+      localStorage.removeItem(ACCESS_TOKEN_KEY);
+      this.router.navigate(['/']);
+  }
+
+}
