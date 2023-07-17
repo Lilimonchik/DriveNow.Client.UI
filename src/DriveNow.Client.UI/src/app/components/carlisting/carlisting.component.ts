2356feac70f87432b17de54a3abf7e127b9e4785
Andrew-CarSharing <obertynskiya@ukr.net>
2023-07-17 16:07:03 +0300

    DV_1_test_0

---
+++ src/DriveNow.Client.UI/src/app/components/carlisting/carlisting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carlisting',
+  templateUrl: './carlisting.component.html',
+  styleUrls: ['./carlisting.component.scss']
+})
+export class CarlistingComponent {
+
+}
