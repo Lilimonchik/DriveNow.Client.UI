c891e6c6acce4061ead5a1637d1de521c9258ed2
Andrew-CarSharing <obertynskiya@ukr.net>
2023-07-17 19:16:47 +0300

    DV_1_cardetails_1

--- src/DriveNow.Client.UI/src/app/app-routing.module.ts
+++ src/DriveNow.Client.UI/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomepageComponent } from "./components/homepage/homepage.component";
 import { CarlistingComponent } from './components/carlisting/carlisting.component';
+import { CardetailsComponent } from './components/cardetails/cardetails.component';
 
 const routes: Routes = [
-  {path: 'homepage', component: HomepageComponent, title: 'home'},
+  {path: '', component: HomepageComponent, title: 'home'},
   {path: 'login', component: LoginComponent, title: 'login'},
   {path: 'signup', component: SignupComponent, title: 'signup'},
-  {path: '', component: CarlistingComponent, title: 'carlisting'}
+  {path: 'carlisting', component: CarlistingComponent, title: 'carlisting'},
+  {path: 'cardetails', component: CardetailsComponent, title: 'cardetails'}
 ];
 
 @NgModule({
