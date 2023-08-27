79dd1a78ebac62cf0b7c0f0c93c52a0dec5aad1f
Andrew-CarSharing <obertynskiya@ukr.net>
2023-08-27 21:31:02 +0300

    DV_1_about_5

--- src/DriveNow.Client.UI/src/app/components/homepage/homepage.component.ts
+++ src/DriveNow.Client.UI/src/app/components/homepage/homepage.component.ts
@@ -8,3 +8,21 @@ import { Component } from '@angular/core';
 export class HomepageComponent {
 
 }
+
+/*
+let counter: number = 1;
+
+setInterval(() => {
+  const radioElement: HTMLInputElement | null = document.getElementById('radio' + counter) as HTMLInputElement;
+  
+  if (radioElement) {
+    radioElement.checked = true;
+  }
+  
+  counter++;
+  
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
+*/
