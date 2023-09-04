c61a6b4e26f4e8eec3f2ce890da5b9096cb6244a
Andrew-CarSharing <obertynskiya@ukr.net>
2023-09-04 10:52:40 +0300

    AM_Problem

--- src/DriveNow.Client.UI/src/app/components/map-car/map-car.component.ts
+++ src/DriveNow.Client.UI/src/app/components/map-car/map-car.component.ts
@@ -150,12 +150,13 @@ export class MapCarComponent implements OnInit {
   public geocoding!: google.maps.Geocoder;
 
   latitude: number = -33.867;
-  longitude:number = 151.206;
+  longitude: number = 151.206;
 
   public coordinates: Coordinates[] = [];
 
   check: boolean = true;
-  infoWindowContent : string = '';
+  infoWindowContent: string = '';
+
   ngOnInit() {
     this.getLocation();
     let loader = new Loader({
@@ -184,10 +185,10 @@ export class MapCarComponent implements OnInit {
       console.log(res);
       this.cars = res;
       for (const car of res) {
-        this.geocoding.geocode({ address: car.address }, (results, status) => {
+        this.geocoding.geocode({address: car.address}, (results, status) => {
           if (status === google.maps.GeocoderStatus.OK && results) {
             const marker = new google.maps.Marker({
-              position: { lat:results[0].geometry.location.lat(), lng: results[0].geometry.location.lng() },
+              position: {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()},
               map: this.map,
               title: car.nameCar // Optional title
             });
@@ -195,9 +196,6 @@ export class MapCarComponent implements OnInit {
               content: this.infoWindowContent
             });
             marker.addListener('click', () => {
-              this.updateInfoWindowContent(car); // Pass the current car to update content
-              infoWindow.setContent(this.infoWindowContent);
-              infoWindow.open(this.map, marker);
             });
           }
         });
@@ -206,72 +204,20 @@ export class MapCarComponent implements OnInit {
     });
     this.getLocation();
   }
-  getLocation(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition((position)=>{
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
             this.latitude = position.coords.latitude;
             this.longitude = position.coords.longitude;
             console.log(this.longitude);
           },
-          (error) =>{
+          (error) => {
             this.latitude = -33.867;
             this.longitude = 151.206;
           })
-    }
-    else{
+    } else {
       console.error("Geolocation is not supported by this browser.");
     }
-  }
-  toggleInfoContent(Car: CarsForMap) {
-    this.check = !this.check;
-    this.updateInfoWindowContent(Car);
-  }
-  updateInfoWindowContent(car: CarsForMap) {
-    if (this.check) {
-      this.infoWindowContent = `<div style="
-    padding: 10px;
-    background-color: white;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    width: 250px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-">
-    <h3 style="
-        margin: 0;
-        font-size: 18px;
-    ">${car.nameCar}</h3>
-    <img src="https://drive-now.s3.eu-west-2.amazonaws.com/${car.photoUrl}" [alt]="${car.nameCar}" width="100%" style="
-        max-height: 150px;
-        margin-top: 10px;
-    ">
-    <p style="
-        font-size: 14px;
-        margin-top: 10px;
-    ">${car.address}</p>
-    <button class="action" (click)="${this.toggleInfoContent(car)}" style="
-        background-color: #FF7A00;
-        color: white;
-        border: none;
-        padding: 5px 10px;
-        border-radius: 3px;
-        cursor: pointer;
-        margin-top: 10px;
-    ">Rent</button>
-</div>
-      `;
-    }
-  else if(!this.check) {
-      this.infoWindowContent = `
-        <div class="main-window">
-        <h3>${car.nameCar}</h3>
-        <img src="https://drive-now.s3.eu-west-2.amazonaws.com/${car.photoUrl}" [alt]="car.carId" width="150" height="100">
-    </div>
-      `;
-    }
-    // Trigger change detection to update the view
-    this.cdRef.detectChanges();
   }
 }
